b815072bf1ad79c70e1300bd208e8aa8da022086
Nganga02 <nganganicholas50@gmail.com>
2025-10-31 12:42:30 +0300

    added the aggregations and window functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,19 @@
+--===========================================
+--USERS BOOKING COUNT
+--===========================================
+
+SELECT bookings.user_id, COUNT(*) FROM bookings GROUP BY bookings.user_id; 
+
+--======================================================
+--RANKING ALL PROPERTIES BOOKING USING A WINDOW FUNCTION
+--======================================================
+
+SELECT 
+property_id, 
+total_bookings, 
+ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS rank
+FROM(
+SELECT property_id, COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+) AS booking_counts;
