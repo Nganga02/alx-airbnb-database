2165c0118bba7d5d951f031c09f073fa2a4c5f94
Nganga02 <nganganicholas50@gmail.com>
2025-10-31 16:19:10 +0300

    added the optimized query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -2,7 +2,28 @@
 -- TASK 04 OPTIMIZING COMPLES QUERIES
 -- ===========================
 
-
+SELECT 
+  b.booking_id, 
+  b.start_date, 
+  b.end_date, 
+  b.total_price, 
+  p.property_id, 
+  p.name, 
+  p.location, 
+  p.pricepernight, 
+  p.host_id, 
+  u.user_id, 
+  u.first_name, 
+  u.last_name, 
+  u.email, 
+  d.payment_id, 
+  d.amount, 
+  d.payment_date, 
+  d.payment_method 
+FROM bookings b 
+JOIN properties p ON b.property_id = p.property_id 
+JOIN users u ON b.user_id = u.user_id 
+LEFT JOIN payments d ON b.booking_id = d.booking_id;
 
 WITH matching_payments AS (
   SELECT 
