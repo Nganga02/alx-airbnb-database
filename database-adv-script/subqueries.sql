8a1c671e776677702c535fc09daa27e02351587b
Nganga02 <nganganicholas50@gmail.com>
2025-10-31 12:06:02 +0300

    added the subqueries file

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,25 @@
+--===========================================
+--IDENTIFYING PROPERTIES WITH A RATING > 4.0
+--===========================================
+
+SELECT * FROM properties WHERE property_id IN (SELECT DISTINCT property_id FROM reviews WHERE reviews.rating > 4);
+
+--===========================================
+--IDENTIFYING USERS WITH MORE THAN 3 BOOKINGS
+--===========================================
+
+
+WITH more_than_3 AS (
+    SELECT user_id, COUNT(*) AS booking_count
+    FROM bookings
+    GROUP BY user_id
+    HAVING COUNT(*) >= 3
+)
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    m.booking_count
+FROM users AS u
+JOIN more_than_3 AS m ON u.user_id = m.user_id;
